25bbe748f412beb977bd6777c78f4925db46d626
patjd0 <99595151+patjd0@users.noreply.github.com>
2022-04-10 20:51:51 +1000

    sudo for main menu

--- src/main.rb
+++ src/main.rb
@@ -3,10 +3,16 @@ require_relative 'hangman'
 require_relative 'math'
 require_relative 'user'
 
+#welcome to Big Brain!
+#Please enter your Name and how old you are.
 
+#user questions Name and Age
 begin User
-
+#beginner score set to 0
 end
 
+#tty-prompt menu to choose games bellow
+#math game, hangman game, trivia
 
+#exit option
 
