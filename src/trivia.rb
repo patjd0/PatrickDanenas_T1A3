f82de5d57e5123956ed2d3fd095ca8416328ad8d
patjd0 <99595151+patjd0@users.noreply.github.com>
2022-04-10 21:17:39 +1000

    create trivia file
    fill file with sudo

---
+++ src/trivia.rb
@@ -0,0 +1,15 @@
+require 'tty-prompt'
+
+#welcome to Big Brain trivia
+#puts ()
+
+#please choose a trivia topic!
+
+  #list of three topics in tty-prompt menu
+  #geography, modern history, ancient history.
+  #each option when chosen will output 10 random questions for the user to answer.
+  #the user will be given a score out of 10.
+
+#output you scored  _ out of 10  
+#puts user back at main menu
+   #update user overall score.
