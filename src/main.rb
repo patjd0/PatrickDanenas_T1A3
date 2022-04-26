a898f09b698e3a15267d84a7696d4e4a0f622ef2
patjd0 <99595151+patjd0@users.noreply.github.com>
2022-04-26 23:52:37 +1000

    menu working

--- src/main.rb
+++ src/main.rb
@@ -5,16 +5,6 @@ require_relative 'menu'
 
 prompt = TTY::Prompt.new
 
-
-#prompt.select('What game would you like to play?') do |menu|
-#  menu.default 3
-
-#  menu.choice 'Hangman', 1 
-#  menu.choice 'Math Big Brain', 2
-#  menu.choice 'Trivia', 3
-#end
-
-
 #welcome to Big Brain!
 #Please enter your Name and how old you are.
 
--- src/menu.rb
+++ src/menu.rb
@@ -1,17 +1,24 @@
 require 'tty-prompt'
+prompt = TTY::Prompt.new
 
-def main_menu
-    answer = @@prompt.select("What Big Brain Game would you like to play?", 
-    ["Hangman", "Math Big Brain", "Trivia"],required: true)
-      case answer
-          when "Hangman"
-            load('hangman.rb')
-          when "Math Big Brain"
-            load('math.rb') 
-          when "Trivia"
-            load('trivia.rb') 
-          else 
-            puts "Error"
-      end
-      
+while true
+
+selection = TTY::Prompt.new.select("What game would you like to play? Please select from the following options:",  cycle: true, marker: '>', echo: false) do |menu|
+ #menu options
+  menu.default 3
+  menu.choice 'Hangman', 1 
+  menu.choice 'Math Big Brain', 2
+  menu.choice 'Trivia', 3
+
+case selection
+when 1 
+   require_relative 'hangman' #calls hangman file.
+when 2
+    require_relative 'math'   #calls math file.
+when 3
+    require_relative 'trivia' #calls trivia file.
+
+
+end
+end
 end      
