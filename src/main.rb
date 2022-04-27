13b9d2870d52cc350a2c33410b8363c9deec3710
patjd0 <99595151+patjd0@users.noreply.github.com>
2022-04-27 21:54:45 +1000

    user class work

--- src/main.rb
+++ src/main.rb
@@ -1,6 +1,23 @@
 require 'tty-prompt'
 require_relative 'user'
-require_relative 'menu'
 prompt = TTY::Prompt.new
 
-puts "welcome to Big Brain!"
+
+
+puts "Welcome To Big Brain!"
+puts "Please enter your name."
+    user_name = gets.chomp.capitalize
+puts "Please enter your age."    
+    age = gets.chomp
+
+
+# Creates a new cat object, and reaffirms to the user their input with a greeting. 
+user = User.new(user_name)
+puts "Welcome #{user.name} and your #{age} year old brain! Let's get training!"
+
+
+require_relative 'menu'
+
+
+#puts "welcome to Big Brain!"
+
--- src/user.rb
+++ src/user.rb
@@ -1,27 +1,19 @@
 require 'tty-prompt'
 prompt = TTY::Prompt.new
 
-
-#user class to take user name and age.
-#this class wil also display a score based on games played.
-
-#result = prompt.collect do
-#  key(:name).ask("What is your name?")
-
-#  key(:age).ask("What is your age?", convert: :int)
-#end
-
-prompt.ask('What is your name?', required: true)
-prompt.ask('How old are you?', required: true)
+#prompt.ask('What is your name?', required: true)
+#prompt.ask('How old are you?', required: true)
 
 class User
-    attr_reader :name, :age
-    attr_writer :score
+    attr_accessor :age, :name, :scores
 
-    def initialize(name, age, score)
-     @name = name
-     @age = age
-     @score = nil
-    end 
-# this class to be input into other pages with require 'user'
-end  
+    def initialize(name)
+        @name = name
+        @age = age    
+        @scores = nil                         
+    end
+
+   # def points([])
+   #     @scores << {[]}
+   # end    
+end
